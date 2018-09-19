1669bebe89585cf458678d8843edc90545920166
qwer11234 <1529833211@qq.com>
2018-09-19 13:23:34 +0800

    1

--- controller/wechart/dynamic.php
+++ controller/wechart/dynamic.php
@@ -2,7 +2,12 @@
 include '../core/db.php';
 class dynamic extends data{
     public function index(){
-        $sql = 'select * from wechart';
+        if(isset($_GET['page'])){
+            $page = (int)$_GET['page'];
+        }else{
+            $page = 1;
+        }
+        $sql = 'select * from wechart order by ctime desc limit 4 offset '.($page-1)*4;
        $data = $this->pdo->query($sql)->fetchAll();
        echo json_encode($data);
     }
@@ -12,23 +17,18 @@ class dynamic extends data{
         }else{
             $urls = 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3559817402,2365570412&fm=27&gp=0.jpg;https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4252978712,688727476&fm=27&gp=0.jpg';
         }
-        if(isset($_GET['time'])){
-            $time = $_GET['time'];
-        }else{
-            $time = '2018年9月11日';
-        }
+
         if(isset($_GET['address'])){
             $address = $_GET['address'];
         }else{
             $address = '学府街平衍路口开通大厦';
         }
-        $stmt = $this->pdo->prepare("insert into wechart (user_avatar,use_name,content,img_urls,pulish_time,pulish_address) values (?,?,?,?,?,?)");
+        $stmt = $this->pdo->prepare("insert into wechart (user_avatar,use_name,content,img_urls,pulish_address) values (?,?,?,?,?)");
         $stmt->bindValue(1,$_GET['user']);
         $stmt->bindValue(2,$_GET['name']);
         $stmt->bindValue(3,$_GET['content']);
         $stmt->bindValue(4,$urls);
-        $stmt->bindValue(5,$time);
-        $stmt->bindValue(6,$address);
+        $stmt->bindValue(5,$address);
         echo $stmt->execute();
 
 
