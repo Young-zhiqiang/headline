bed7f5c7ab0314ee942eb75c1cd4e89abea6df32
qwer11234 <1529833211@qq.com>
2018-09-20 21:52:32 +0800

    k

--- controller/wechart/dynamic.php
+++ controller/wechart/dynamic.php
@@ -1,5 +1,39 @@
 <?php
 include '../core/db.php';
+function https_request($url,$data=""){
+    // 开启curl
+    $ch=curl_init();
+    // 设置传输选项
+    // 设置传输地址
+    curl_setopt($ch, CURLOPT_URL, $url);
+    // 以文件流的形式返回
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    /**/
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //不验证证书下同
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+
+    if ($data) {
+        // 以post方式
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    }
+
+    $headers = array("Content-type: application/json;charset=UTF-8","Accept: application/json","Cache-Control: no-cache", "Pragma: no-cache");
+
+    curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+
+    // 发送curl
+    $request=curl_exec($ch);
+    $tmpArr=json_decode($request,TRUE);
+    if (is_array($tmpArr)) {
+        return $tmpArr;
+    }else{
+        return $request;
+    }
+    // 关闭资源
+    curl_close($ch);
+}
 class dynamic extends data{
     public function index(){
         if(isset($_GET['page'])){
@@ -12,26 +46,22 @@ class dynamic extends data{
        echo json_encode($data);
     }
     public function insert(){
-        if(isset($_GET['urls'])){
-            $urls = $_GET['urls'];
-        }else{
-            $urls = 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3559817402,2365570412&fm=27&gp=0.jpg;https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4252978712,688727476&fm=27&gp=0.jpg';
-        }
-
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wxdc03c85bc1164b44&secret=0705ab1ea1c63857441f3ed292ce884a&js_code='.$_GET['code'].'&grant_type=authorization_code';
+        $relaut = https_request($url);
         if(isset($_GET['address'])){
             $address = $_GET['address'];
         }else{
             $address = '学府街平衍路口开通大厦';
         }
-        $stmt = $this->pdo->prepare("insert into wechart (user_avatar,use_name,content,img_urls,pulish_address) values (?,?,?,?,?)");
-        $stmt->bindValue(1,$_GET['user']);
-        $stmt->bindValue(2,$_GET['name']);
-        $stmt->bindValue(3,$_GET['content']);
-        $stmt->bindValue(4,$urls);
-        $stmt->bindValue(5,$address);
-        echo $stmt->execute();
-
-
+        $stmt = $this->pdo->prepare("insert into wechart (openid,user_avatar,use_name,content,img_urls,pulish_address) values (?,?,?,?,?,?)");
+        $stmt->bindValue(1,$relaut['openid']);
+        $stmt->bindValue(2,$_GET['user']);
+        $stmt->bindValue(3,$_GET['name']);
+        $stmt->bindValue(4,$_GET['content']);
+        $stmt->bindValue(5,$_GET['urls']);
+        $stmt->bindValue(6,$address);
+        $stmt->execute();
+        echo $relaut['openid'];
     }
     public function upload(){
 //        print_r($_FILES);
@@ -39,6 +69,10 @@ class dynamic extends data{
         $file_name = $_FILES['g']['name'];
         $dist = './assets/wechart/'.$file_name;
         move_uploaded_file($src, $dist);
-        echo 'http://www.news.com/assets/wechart/'.$file_name;
+        echo 'https://blacknight.applinzi.com/assets/wechart/'.$file_name;
+    }
+    public function user_feed(){
+        $user = $this->pdo->query("select * from wechart where openid=".$_GET['openid'])->fetchAll();
+        echo json_encode($user);
     }
 }
